df867ce7aba54b0f00d457b282e6b28963883e63
David <davvan@pdx.edu>
2025-04-24 14:31:05 -0700

    info changed placeholder

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,23 +6,25 @@ import {
   faYoutube,
 } from '@fortawesome/free-brands-svg-icons';
 import SocialButton from './SocialButton'; // Adjust path as needed
+import { Mail } from 'lucide-react';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 export default function Footer() {
   return (
-    <footer className='py-8 border-zomi-green-less'>
-      <div className='max-w-5xl mx-auto px-6'>
-        {/* <div className='flex flex-col md:flex-row justify-center gap-12 text-center md:text-left mb-10'>
+    <footer className='py-8 mx-auto'>
+      <div className='max-w-5xl px-6'>
+        <div className='flex flex-col md:flex-row justify-start gap-12 text-center md:text-left mb-10'>
           <div>
             <p className='font-semibold'>Address:</p>
             <p>123 Main St</p>
             <p>Portland, OR 97234</p>
           </div>
           <div>
-            <p className='font-semibold'>Services:</p>
-            <p>Youth Service: Saturday 7:30 pm</p>
-            <p>Regular Service: Sunday 4:00 pm</p>
+            <p className='font-semibold'>Phone:</p>
+            <p>En Khai: +1 503-456-789</p>
+            <p>En Khai: +1 503-456-789</p>
           </div>
-        </div> */}
+        </div>
 
         <div className='flex justify-center gap-6 flex-wrap'>
           <SocialButton
