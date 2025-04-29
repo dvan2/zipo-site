36c5dd60cccb28a2ec0bd5736b4b16cda8d06f13
David <davvan@pdx.edu>
2025-04-29 13:37:19 -0700

    navbar adjustment big screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,28 +21,28 @@ export default function Navbar() {
 
   return (
     <nav className='bg-background'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-        <div className='flex items-center justify-between h-32 py-8 relative'>
+      <div className='max-w-7xl mx-auto px-2 sm:px-6'>
+        <div className='flex items-center justify-between lg:h-36 h-26 relative'>
           <div className='flex-shrink-0'>
             <Link href='/' className='flex items-center'>
               <Image
                 src='/images/ziporound.png'
                 alt='ZIPO logo'
-                width={48}
-                height={48}
+                width={64}
+                height={64}
               />
             </Link>
           </div>
 
           {/* Desktop Menu */}
-          <div className='hidden md:flex absolute left-1/2 transform -translate-x-1/2 space-x-16 whitespace-nowrap text-base font-light text-gray-700'>
+          <div className='hidden md:flex absolute left-1/2 transform -translate-x-1/2 space-x-12 lg:space-x-16 whitespace-nowrap text-base font-light text-gray-700'>
             {navItems.map(({ label, href }) => (
               <Link
                 key={href}
                 href={href}
                 className={`nav-link relative ${
                   pathname === href
-                    ? 'text-blue-600 font-semibold after:w-full'
+                    ? 'text-zomi-green font-semibold after:w-full'
                     : 'text-gray-700'
                 }`}
               >
@@ -88,8 +88,8 @@ export default function Navbar() {
                 href={href}
                 onClick={() => setIsOpen(false)}
                 className={`text-2xl font-semibold ${
-                  pathname === href ? 'text-blue-600' : 'text-gray-700'
-                } hover:text-blue-600 transition`}
+                  pathname === href ? 'text-zomi-green' : 'text-gray-700'
+                } hover:text-zomi-green transition`}
               >
                 {label}
               </Link>
